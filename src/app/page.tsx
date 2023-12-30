fa935121207693332226334f9d54991b5a8419b1
Ahmad Orucov <Axmedik53755@gmail.com>
2023-12-31 02:25:27 +0400

    up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 // const protocal = process?.env.NODE_ENV === "development" ? "http://" : "https://"
 async function getData() {
-  const res = await fetch(`${process.env.BASE_API_URL}api/allData`, { cache: "no-store" })
+  const res = await fetch(`${process.env.BASE_API_URL}api/allData`)
   return res.json()
 }
 
