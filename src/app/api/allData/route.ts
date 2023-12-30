aa0c0cf52f8db6656e9df448fac5db7220008d00
Ahmad Orucov <Axmedik53755@gmail.com>
2023-12-31 02:45:29 +0400

    up

--- src/app/api/allData/route.ts
+++ src/app/api/allData/route.ts
@@ -1,9 +1,29 @@
-import excuteQuery from "../db/db";
+// import executeQuery from "../db/db";
+// import { NextResponse } from "next/server";
+//
+// export const GET = async (req: Request, res: Response) => {
+//   try {
+//     const allData = await executeQuery({
+//       query: `SELECT all_products.*,category.slug ,city.name AS cityName FROM all_products
+//        INNER JOIN category ON
+//        all_products.categoryId = category.id
+//        INNER JOIN city ON all_products.cityId = city.id
+//        ORDER BY all_products.createdAt DESC`,
+//       values: [],
+//     });
+//
+//     return NextResponse.json({success:allData });
+//   } catch (error) {
+//     return NextResponse.json({err:"xeta bas verdi"});
+//   }
+// };
+//
+import  executeQuery  from "../db/db";
 import { NextResponse } from "next/server";
 
-export const GET = async (req: Request, res: Response) => {
+export const GET = async (req:Request, res:Response) => {
   try {
-    const allData = await excuteQuery({
+    const allData = await executeQuery({
       query: `SELECT all_products.*,category.slug ,city.name AS cityName FROM all_products
        INNER JOIN category ON
        all_products.categoryId = category.id
@@ -12,9 +32,9 @@ export const GET = async (req: Request, res: Response) => {
       values: [],
     });
 
-    return NextResponse.json({success:allData });
+    return NextResponse.json({ success: allData });
   } catch (error) {
-    return NextResponse.json({err:"xeta bas verdi"});
+    console.error("Database error:", error);
+    return NextResponse.json({ err: "Veritabanı hatası" });
   }
 };
-
