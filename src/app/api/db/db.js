63535cbb53a1cf9fb6f2498652236371c498af01
Ahmad Orucov <Axmedik53755@gmail.com>
2023-12-31 02:32:07 +0400

    up

--- src/app/api/db/db.js
+++ src/app/api/db/db.js
@@ -1,22 +1,22 @@
 import mysql from "serverless-mysql";
 
 const db = mysql({
-  config: {
-    host:"45.80.153.6",
-    port:3306,
-    database:"kupo_mynewdb",
-    user:"kupo_mynewdb",
-    password:"b6JPnuc!+0-wK^of"
-
-  },
   // config: {
-  //   host:process.env.NEXT_HOST_NAME,
-  //   port:process.env.NEXT_PORT_NAME,
-  //   database:process.env.NEXT_DATABASE_NAME,
-  //   user:process.env.NEXT_USER_NAME,
-  //   password:process.env.NEXT_PASSWORD
+  //   host:"45.80.153.6",
+  //   port:3306,
+  //   database:"kupo_mynewdb",
+  //   user:"kupo_mynewdb",
+  //   password:"b6JPnuc!+0-wK^of"
   //
   // },
+  config: {
+    host:process.env.NEXT_HOST_NAME,
+    port:process.env.NEXT_PORT_NAME,
+    database:process.env.NEXT_DATABASE_NAME,
+    user:process.env.NEXT_USER_NAME,
+    password:process.env.NEXT_PASSWORD
+
+  },
 });
 
 export default async function executeQuery({ query, values }) {
