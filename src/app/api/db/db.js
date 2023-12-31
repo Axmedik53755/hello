46d6dab8737a8392b31197db5fd4ff784dff979f
Ahmad Orucov <Axmedik53755@gmail.com>
2023-12-31 05:33:22 +0400

    up

--- src/app/api/db/db.js
+++ src/app/api/db/db.js
@@ -1,34 +1,13 @@
-// import mysql from "serverless-mysql";
-//
-// const db = mysql({
-//   config: {
-//     host:process.env.NEXT_HOST_NAME,
-//     port:process.env.NEXT_PORT_NAME,
-//     database:process.env.NEXT_DATABASE_NAME,
-//     user:process.env.NEXT_USER_NAME,
-//     password:process.env.NEXT_PASSWORD
-//
-//   },
-// });
-//
-// export default async function executeQuery({ query, values }) {
-//   try {
-//     const results = await db.query(query, values);
-//     await db.end();
-//     return results;
-//   } catch (error) {
-//     return { error };
-//   }
-// }
 import mysql from "mysql2";
 
 const db = mysql.createConnection({
-  host: "45.80.153.6",
-  port: 3306,
-  database:"kupo_mynewdb",
-  user: "kupo_mynewdb",
-  password:"b6JPnuc!+0-wK^of"
+    host:process.env.NEXT_HOST_NAME,
+    port:process.env.NEXT_PORT_NAME,
+    database:process.env.NEXT_DATABASE_NAME,
+    user:process.env.NEXT_USER_NAME,
+    password:process.env.NEXT_PASSWORD
 });
+
 
 export default async function executeQuery({ query, values }) {
   try {
--- src/app/api/star/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server"
-
-
-
-export const GET = () => {
-    console.log("hello")
-    return NextResponse.json({ msg: "Salam 1 ci sehifeden backend-den", success: true })
-}
