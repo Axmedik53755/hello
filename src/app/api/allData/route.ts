ee650ec4e3ed578ec31fa646679950a469418e2f
Ahmad Orucov <Axmedik53755@gmail.com>
2023-12-31 07:00:23 +0400

    up

--- src/app/api/allData/route.ts
+++ src/app/api/allData/route.ts
@@ -1,23 +1,3 @@
-// import executeQuery from "../db/db";
-// import { NextResponse } from "next/server";
-//
-// export const GET = async (req: Request, res: Response) => {
-//   try {
-//     const allData = await executeQuery({
-//       query: `SELECT all_products.*,category.slug ,city.name AS cityName FROM all_products
-//        INNER JOIN category ON
-//        all_products.categoryId = category.id
-//        INNER JOIN city ON all_products.cityId = city.id
-//        ORDER BY all_products.createdAt DESC`,
-//       values: [],
-//     });
-//
-//     return NextResponse.json({success:allData });
-//   } catch (error) {
-//     return NextResponse.json({err:"xeta bas verdi"});
-//   }
-// };
-//
 import  executeQuery  from "../db/db";
 import { NextResponse } from "next/server";
 
@@ -32,7 +12,7 @@ export const GET = async (req:Request, res:Response) => {
       values: [],
     });
 
-    return NextResponse.json({ success: allData });
+    return NextResponse.json({ success: true,allData });
   } catch (error) {
     console.error("Database error:", error);
     return NextResponse.json({ err: "Veritabanı hatası", error }); // Hata bilgisi ekleyin
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,28 @@
-
 import Link from 'next/link'
-
 // const protocal = process?.env.NODE_ENV === "development" ? "http://" : "https://"
 async function getData() {
-  const res = await fetch(`${process.env.BASE_API_URL}api/allData`)
-  return res.json()
+
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts`,{cache:"no-store"})
+    const data = await res.json();
+     return data 
+   
 }
 
+
 export default async function Home() {
-  const data = await getData()
+const data=await getData()
+
   console.log("console",data)
   return (
     <div>
       <div> Hello from</div>
         <div>
             {
-                data?.success?.length&&    data?.success?.map((item:any,index:any)=>{
+             data.map((item:any,index:any)=>{
                     return (
                         <div key={index}>
                             <div key={item.id}>
-                                {item.name}
+                                {item.title}
                             </div>
                         </div>
                     )
